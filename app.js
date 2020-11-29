a459f69fe61ff9890508d7634f8676deb73e9f75
jsalcedo10 <42553096+jsalcedo10@users.noreply.github.com>
2020-11-29 15:22:50 -0800

    subiendo cambios

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
+var {createPreference} = require('./mercadopago');
 
 var app = express();
-
-
-// SDK de Mercado Pago
-const mercadopago = require ('mercadopago');
-
-// Agrega credenciales
-mercadopago.configure({
-  access_token: 'TEST-6304899592642937-112601-bf8e7e99cba0ff3ac1f61727f0231e0a-240313692'
-});
-
-// Crea un objeto de preferencia
-let preference = {
-  items: [
-    {
-      title: 'PRODUCTO',
-      unit_price: 100,
-      quantity: 1,
-    }
-  ]
-};
-
-mercadopago.preferences.create(preference)
-.then(function(response){
-// Este valor reemplazará el string "<%= global.id %>" en tu HTML
-  global.id = response.body.id;
-  console.log(response.body.sandbox_init_point)
-}).catch(function(error){
-  console.log(error);
-});
-
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -45,12 +16,28 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+// app.get('/detail', function (req, res) {
+//     res.render('detail', {...req.query,mpid:global.id});
+// });
+
+app.get('/detail', async function (req, res) {
+    let id = await createPreference(req.query)
+    .then(id=> id)
+    .catch(err=> err)
+    console.log(req.query)
+    res.render('detail', {...req.query, idmp: global.id});
+});
+app.get('/success', function (req, res) {
+    res.render('success', req.query);
+});
+app.get('/pending', function (req, res) {
+    res.render('pending', req.query);
+});
+app.get('/failure', function (req, res) {
+    res.render('failure', req.query);
 });
 
-
-app.post('/cho', function (req, res) {
+app.post('/checkout', function (req, res) {
     console.log(req.params);
     console.log(req.query);
     res.status(200).json({ok:true});
---
+++ config.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://jsalcedo10-mp-ecommerce-node.herokuapp.com'
+
+module.exports = { baseUrl }
